5c312cb18001c4da50c21033bb82d566b0b5a684
soma1701 <somasingh1701@gmail.com>
2017-07-20 07:36:44 +0530

    Two dimensional array

--- src/com/soma/logical/TwoDArr.java
+++ src/com/soma/logical/TwoDArr.java
@@ -6,30 +6,52 @@ public class TwoDArr {
 	public static void main(String[] a){
 		Scanner sc=new Scanner (System.in);
 		System.out.println("Enter no of rows:-");
-		int r=sc.nextInt();
+		int row=sc.nextInt();
 		System.out.println("Enter no of cols:-");
-		int c=sc.nextInt();
-		int[][] b=new int[r][c];
-		System.out.println("Enter elements:-"+(r*c));
-        for(int i=0;i<r;i++)
-        {
-        	for(int j=0;j<c;j++){
-        		b[i][j]=sc.nextInt();
-        		
-        	}
-        }
-        System.out.println("Print two d arr:");
-        for(int i=0;i<r;i++){
-        	for(int j=0;j<c;j++){
+		int column=sc.nextInt();
+		System.out.println("Enter your choice");
+		int choice;
+		choice=sc.nextInt();
+		switch(choice){
+			case 1:
+				insertInteger(row,column,sc);
+				break;
+			case 2:
+				insertDouble();
+				break;
+			case 3:
+				insertBoolean();
+				break;
+			default:
+				System.err.println("You have given wrong input");
+		}
+	}
+
+	public static void insertInteger(int row, int column, Scanner sc) {
+		int[][] b=new int[row][column];
+		boolean isExceptionOccured = false;
+		do{
+			try{
+				System.out.println("Enter elements:-"+(row*column));
+		        for(int i=0;i<row;i++)
+		        {
+		        	for(int j=0;j<column;j++){
+		        		b[i][j]=sc.nextInt();
+		        	}
+		        }
+		        isExceptionOccured = false;
+			}catch(Exception e){
+//				e.printStackTrace();
+				isExceptionOccured = true;
+				System.out.println("Please enter integer value only");
+			}
+		}while(isExceptionOccured);
+		System.out.println("Print two d arr:");
+        for(int i=0;i<row;i++){
+        	for(int j=0;j<column;j++){
         		System.out.print(b[i][j]+" ");
         	}
         	   System.out.println();
         }
-     
-		
-		
-		
-		
 	}
-
 }
