128521bc8f2a3f852d4f545388e2989500c1abcd
soma1701 <khansalman752@gmail.com>
2017-07-25 18:40:38 +0530

    Utility and Programmes

--- src/com/soma/logical/main/UtilityMain.java
+++ src/com/soma/logical/main/UtilityMain.java
@@ -7,8 +7,12 @@ import com.soma.logical.util.Utility;
 
 public class UtilityMain{
 	public static void main(String[] a){
+		long startTime=0,endTime=0;
 	    Utility u=new Utility();
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter integer for binarysearch:");
+		
+		
 		
 	}	
 }
